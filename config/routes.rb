8561dcbc619775b437beadb07b659dc2840bf344
Alex Williams <ajdw1987@gmail.com>
2019-06-04 18:23:44 -0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+=begin Rails.application.routes.draw do
+   For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "events" => "events#index"
+=end 
+
+Rails.application.routes.draw do
+  root "movies#index"
+  # get "movies"     => "movies#index"
+  # get "movies/new" => "movies#new"
+  # get "movies/:id" => "movies#show", as: "movie"
+  # get "movies/:id/edit" => "movies#edit", as: "edit_movie"
+  # patch "movies/:id" => "movies#update"
+
+  # uses a built-in Rails convention to dynamically define a bunch of routes
+  resources :movies
+
+end
