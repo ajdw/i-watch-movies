8795caf211e008e0cca6b885bd9a23ede2bbeadb
Alex Williams <ajdw1987@gmail.com>
2019-06-12 22:44:06 -0700

    added review form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,36 @@
 class ReviewsController < ApplicationController
+before_action :set_movie
 
 	def index
-		# @movies is a variable that's assigned to Movie with a method that will find, by parameters, the movie_id
-		@movie = Movie.find(params[:movie_id])
 		# @reviews is a variable that's assigned to that specific movie and its reviews
 		@reviews = @movie.reviews
 	end
+
+	def new
+		# We are assigning the @review variable equal to 
+		@review = @movie.reviews.new
+	end
+
+	def create
+		@review = @movie.reviews.new(review_params)
+		if @review.save
+			redirect_to movie_reviews_path(@movie), 
+							notice: "Thank you for your review!"
+		else
+			render :new
+		end
+	end
+
+	private 
+	def review_params
+		params.require(:review).permit(:name, :comment, :stars)
+	end
+
+	def set_movie
+		# @movies is a variable that's assigned to Movie with a method that will find, by parameters, the movie_id
+		@movie = Movie.find(params[:movie_id])
+	end
+
+
 
 end
